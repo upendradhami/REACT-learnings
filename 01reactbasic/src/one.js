b867944c3179d27bacea6026d16b313a69574dea
Upendra Dhami <upendradhami.becomp@2079.edu.np>
2025-09-04 17:01:49 +0545

    known about how to add new components in react and vite

---
+++ 01reactbasic/src/one.js
@@ -0,0 +1,7 @@
+function One(){
+  return(
+  <h1> creating new component named one </h1>
+);
+}
+
+export default One;
---
+++ 01viteproject/src/New.jsx
@@ -0,0 +1,8 @@
+ 
+ function New() {
+  return (
+    <b> This is new component in the vite project added from new.jsx similar to main.jsx </b>
+  )
+ }
+
+ export default New;
